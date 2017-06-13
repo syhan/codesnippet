30bc812239fd52206c7aaf1e9ea6cbebb1a7b8e9
syhan <yihan.song@sap.com>
2017-06-13 18:37:09 +0800

    Make the monitor script more robust

--- jzh.sh
+++ jzh.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
+[[ -x `which tesseract` ]]
+[ $? -ne 0 ] && exit
 
 ID=<ID>
 PW=<PW>
 
+echo -ne "Start session..."
 curl 'http://jzh.12333sh.gov.cn/jzh/' --cookie-jar session.cookie --silent --output /dev/null
+echo -ne " [Done]\n"
 
-while [[ $(curl 'http://jzh.12333sh.gov.cn/jzh/image.jsp?Math.random();' --cookie session.cookie -o verify.png --silent && tesseract verify.png code -l eng && cat code.txt | wc -m) != 6 ]]
+while true
 do
-    echo "retry"
+    echo -ne "Get verify code..."
+    curl 'http://jzh.12333sh.gov.cn/jzh/image.jsp?Math.random();' --cookie session.cookie -o verify.png --silent
+    echo -ne " [Done]\n"
+
+    echo -ne "Start to reconginze verify code"
+    tesseract verify.png code -l eng &> /dev/null
+    echo -ne " $(cat code.txt) [Done]\n"
+
+    [[ $(cat code.txt | wc -m) != 6 ]] || break
 done
 
+echo -ne "Start login..."
 curl 'http://jzh.12333sh.gov.cn/jzh/userLoginAction!login.action' --cookie session.cookie --data "loginInfo.login_username=$ID&loginInfo.login_password=$PW&loginInfo.login_type=0&Rand=`cat code.txt`&radioCode=1&Submit=+" --silent --output /dev/null
+echo -ne " [Done]\n"
+
+echo -ne "Check the Hukou progress..."
 curl 'http://jzh.12333sh.gov.cn/jzh/userLoginAction!LoginPerson.action' --cookie session.cookie --data "Button=%E6%88%91%E6%8E%A5%E5%8F%97&username=$ID" --silent | grep '<td  align="center">&nbsp;' -A 5 | sed -r 's/<td\s+align="center">&nbsp;//g' | sed -r 's/<\/td>//g'
 
 rm verify.png
